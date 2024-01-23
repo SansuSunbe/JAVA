2695aa0c9bd47615d5058ffd9ccff877adddadab
SansuSunbe <149463418+SansuSunbe@users.noreply.github.com>
2024-01-23 20:12:45 +0900

    Add files via upload

---
+++ work/Day15/src/day15/ArrayExFruit.java
@@ -0,0 +1,24 @@
+package day15;
+
+import java.util.Scanner;
+
+public class ArrayExFruit {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String fruits [] = new String[3];
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println("주문할 과일 : ");
+			fruits[i] = sc.next();
+		}
+		System.out.println("--- 주문 받은 과일 ---");
+		
+		for(String eachFruit : fruits) {
+			System.out.print(eachFruit + " ");
+		}
+		
+		sc.close();
+		
+	}
+}
---
+++ work/Day15/src/day15/ArrayForEach.java
@@ -0,0 +1,17 @@
+package day15;
+
+public class ArrayForEach {
+	public static void main(String[] args) {
+		
+		String a [] = {"Java", "Hello", "Programing"};
+		for(String i : a) {
+			System.out.println(i);
+		}
+		
+		int b [] = {1, 2, 3, 4, 5};
+		for(int i : b){
+			System.out.print(i);
+		}
+		
+	}
+}
