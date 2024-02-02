69e57637e2a0acd3d30fa0158f3718dcc6dfa018
SansuSunbe <149463418+SansuSunbe@users.noreply.github.com>
2024-02-02 17:42:32 +0900

    Add files via upload

---
+++ work/Day40/src/day40/PhoneBookMain.java
@@ -0,0 +1,78 @@
+package day40;
+
+import java.util.*;
+
+public class PhoneBookMain {
+	// 전역변수로 사용하기 위해 static을 붙인다.
+	static HashMap<String, String> map = new HashMap<>();
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int choice;
+
+		while (true) {
+			System.out.println("전화번호부");
+			System.out.println("1.고객 정보 저장");
+			System.out.println("2.고객 정보 검색");
+			System.out.println("3.고객 정보 출력");
+			System.out.println("4.끝내기");
+			System.out.println("번호 입력 : ");
+			choice = sc.nextInt();
+			if (choice == 1) {
+				System.out.println("--- 고객 정보 저장 ---");
+				insert();
+			} else if (choice == 2) {
+				System.out.println("--- 고객 정보 검색 ---");
+				search();
+			} else if (choice == 3) {
+				System.out.println("--- 고객 정보 출력 ---");
+				show();
+			} else if (choice == 4) {
+				System.out.println("프로그램 종료");
+				break;
+			} else {
+				System.out.println("잘못된 입력");
+			}
+		}
+
+	}
+
+	public static void insert() {
+		String name, phone;
+		System.out.println("저장할 고객의 이름 : ");
+		name = sc.next();
+		// name은 Key이므로, 중복 허용불가
+		if (map.containsKey(name)) {
+			System.out.println("이미 존재하는 이름");
+			return;
+		} else {
+			System.out.println("저장할 고객의 번호 : ");
+			phone = sc.next();
+			map.put(name, phone);
+			System.out.println("저장 완료");
+		}
+	}
+
+	public static void search() {
+		String searchName;
+		System.out.println("검색할 이름 : ");
+		searchName = sc.next();
+		// searchName이 map에 존재해야 검색 가능
+		if (map.containsKey(searchName)) {
+			System.out.println("검색 완료");
+			System.out.println("검색된 전화 번호 : " + map.get(searchName));
+		} else {
+			System.out.println("검색된 이름 없음");
+		}
+	}
+
+	public static void show() {
+		Set<String> keyset = map.keySet();
+		Iterator i = keyset.iterator();
+		while (i.hasNext()) {
+			String curName = (String) i.next();
+			System.out.println("name : " + curName + ", phone : " + map.get(curName));
+		}
+	}
+}
